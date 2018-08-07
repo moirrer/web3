6418175a1c26cf281ecb431cc21474ccfe7f9317
Moises Wendt <moises@ceisc.com.br>
2018-08-07 20:10:55 -0300

    Fix on arguments array, commented gas to run test and add "0x" for bytecode test

--- deploy.js
+++ deploy.js
@@ -24,23 +24,24 @@ function log(){
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
 
-    log('Interface: ', interface);
-    log('Bytecode: ', bytecode);
-    log('Accounts: ', accounts);
+    //log('Interface: ', interface);
+    //log('Bytecode: ', bytecode);
+    //log('Accounts: ', accounts);
     log('Attempting to deploy from account', accounts[0]);
 
     try{
         const result = await new web3.eth
                         .Contract(JSON.parse(interface))
                         .deploy({
-                            data : bytecode,
-                            arguments : 'teste' 
+                            // data : bytecode,
+                            data : '0x' + bytecode,
+                            arguments : ['teste']
                         })
                         .send({
-                            gas: '1000000',
+                            //gas: '1000000',
                             from: accounts[0]
                         });
-                        
+
         log('Contract deployed to ', result.options.address);
     } 
     catch (error) {
